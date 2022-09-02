5423dcb1447693d88b3e3e9f522ac2411d3d0081
mayank.gupta <mayank.gupta@ihsmarkit.com>
2022-09-02 15:13:49 +0530

    spring boot starter

---
+++ src/test/java/com/example/demo1/Demo1ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo1;
+
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
